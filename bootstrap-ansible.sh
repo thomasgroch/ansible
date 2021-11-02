a623203600885823cdc9832492ab3fbbd6ca0b55
Thomas Letsch Groch <thomas.groch@gmail.com>
2021-11-02 00:30:39 -0300

    fix

--- bootstrap-ansible.sh
+++ bootstrap-ansible.sh
@@ -1,10 +1,8 @@
-[[ ! -e ~/.password-store ]] || git clone https://gitlab.com/thomas.groch/password-store.git ~/.password-store && \
-	if [[ -n $(which xclip) ]]; then
-		sudo pacman -S --noconfirm xclip
-	fi
+if [[ ! -e ~/.password-store ]]; then
+	git clone https://gitlab.com/thomas.groch/password-store.git ~/.password-store
 	# Import my GnuPG and OpenSSH keys
 	. /run/media/tg/safe/run.sh
-
+fi
 if [[ -z $(which ansible-pull) ]]; then # if are not installed
 	if [[ -n $(which pacman) ]]; then # if are installed
 		sudo pacman -S --noconfirm ansible
@@ -12,6 +10,5 @@ if [[ -z $(which ansible-pull) ]]; then # if are not installed
 		sudo apt install -y ansible
 	fi
 fi
-if [[ -n $(which ansible-pull) ]]; then
-	sudo ansible-pull --url https://github.com/thomasgroch/ansible --limit $(cat /etc/hostname).local
-fi
+
+sudo ansible-pull --url https://github.com/thomasgroch/ansible --limit $(cat /etc/hostname).local
